a2161fa4646d694129f2dc4554f80d7e2ad35727
lartweib <a.federicodaniel@gmail.com>
2021-08-22 20:43:01 -0300

    comentando codigo

--- Flask/main.py
+++ Flask/main.py
@@ -1,18 +1,24 @@
 from flask import Flask
 
+# Crear la app de flask
 app = Flask(__name__)
 
+# Definir las rutas con el metodo route
 @app.route('/')
 def index():
     return 'Aprendiendo Flask'
 
+# Podemos pasar parametros en la ruta '/<>'
+# Tambien podemos definir el formato del parametro 'string:'
 @app.route('/informacion/<string:nombre>/<int:apellidos>')
+# Para recibir la informacion pasamos los parametros a la funcion
 def informacion(nombre, apellidos):
     return f'''
                 <h1>Pagina de informacion</h1>'
                 <p>Esta es la pagina de informacion</p>
                 <h3>Bienvenido, {nombre} {apellidos}</h3>
     '''
+
 @app.route('/contacto')
 def contacto():
     return '<h1>Pagina de contacto</h1>'
@@ -21,6 +27,6 @@ def contacto():
 def lenguajes():
     return '<h1>Pagina de lenguajes</h1>'
 
-
+# Esto es para identificar el fichero principal
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True) # Debug True indica que estamos en modo debug
