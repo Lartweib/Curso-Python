fb26979a165b466f2f487f89ec2d5118c1b7bd49
lartweib <a.federicodaniel@gmail.com>
2021-08-22 21:51:19 -0300

    clase 2

--- Flask/main.py
+++ Flask/main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 
 # Crear la app de flask
 app = Flask(__name__)
@@ -8,25 +8,39 @@ app = Flask(__name__)
 def index():
     return 'Aprendiendo Flask'
 
+#Defino la misma ruta para hacer que el parametro sea opcional
+@app.route('/informacion')
 # Podemos pasar parametros en la ruta '/<>'
 # Tambien podemos definir el formato del parametro 'string:'
-@app.route('/informacion/<string:nombre>/<int:apellidos>')
+@app.route('/informacion/<string:nombre>')
 # Para recibir la informacion pasamos los parametros a la funcion
-def informacion(nombre, apellidos):
+def informacion(nombre = None):
+    
+    texto = ""
+    if nombre != None:
+        texto = f'<h3>Bienvenido, {nombre}</h3>'
+    
     return f'''
-                <h1>Pagina de informacion</h1>'
+                <h1>Pagina de informacion</h1>
                 <p>Esta es la pagina de informacion</p>
-                <h3>Bienvenido, {nombre} {apellidos}</h3>
+                {texto}
     '''
 
 @app.route('/contacto')
-def contacto():
+@app.route('/contacto/<redireccion>')
+def contacto(redireccion = None): 
+
+    if redireccion is not None:
+        #Podemos redireccionar paginas utilizando redirect
+        return redirect(url_for('lenguajes'))
+
     return '<h1>Pagina de contacto</h1>'
 
-@app.route('/lenguajes')
+@app.route('/lenguajes-de-programacion')
 def lenguajes():
     return '<h1>Pagina de lenguajes</h1>'
 
 # Esto es para identificar el fichero principal
 if __name__ == '__main__':
-    app.run(debug=True) # Debug True indica que estamos en modo debug
+    # Debug True indica que estamos en modo debug
+    app.run(debug=True)
